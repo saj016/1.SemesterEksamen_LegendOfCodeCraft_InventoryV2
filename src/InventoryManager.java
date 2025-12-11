4914347a85a47f93ba9c4126c17c609e1492c488
amali <amaliegp@gmail.com>
2025-12-11 22:29:27 +0100

    commit

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -4,6 +4,7 @@ import exceptions.InventoryWeightLimitReachedException;
 import exceptions.NoEmptySlotsAvailableException;
 import inventory.Inventory;
 import inventory.InventorySlot;
+import items.Consumable;
 import items.Item;
 import items.Sword;
 
@@ -12,7 +13,7 @@ import java.util.Arrays;
 public class InventoryManager {
     private Inventory inventory;
 
-    public InventoryManager(){
+    public InventoryManager() {
         this.inventory = new Inventory();
     }
 
@@ -35,8 +36,8 @@ public class InventoryManager {
     }
 
     public void removeItemFromSlot(int slot) {
-       int index = slot - 1;
-       inventory.removeItemFromSlot(index);
+        int index = slot - 1;
+        inventory.removeItemFromSlot(index);
     }
 
     //TODO searchItem
@@ -49,17 +50,48 @@ public class InventoryManager {
     TODO - find alle af det, man søger efter
      */
 
-    public void searchForItem(String searchParameter){
-       InventorySlot[] items = this.inventory.getItems();
-        if(Arrays.asList(items).contains(searchParameter)){
-            System.out.println("WORKS!! - SLET MIGGG!!!!");
-        } else
-            System.out.println("NOT WORKING -- DELETE DELETE ");
+    public String searchForItem(String searchParameter) {
+        InventorySlot[] items = this.inventory.getItems();
+        int amountInInventory = 0;
+        String startMessage = ("You are searching for: " + searchParameter) + " and the system found "; //TODO evt lav en bedre promt
+        String list = "";
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getItem() != null) {
+                if (items[i].getItem().getName().contains(searchParameter)) {
+                    amountInInventory += 1;
+                    if (items[i].getItem() instanceof Consumable) {
+                        list += ("Slot " + (i+1) + ": " + items[i].getItem().getName() + " (" + items[i].getQuantity() + ")\n");
+                    } else {
+                        list += ("Slot " + (i+1) + ": " + items[i].getItem().getName() + "\n");
+                    }
+                }
+            }
+        }
+        return startMessage + amountInInventory + "\n" + list;
     }
 
-    /*
-    boolean dataSort(Items[])
-    - bubblesort
-    return boolean if sort successful
-     */
+
+
+    void dataSort() {
+        InventorySlot[] items = this.inventory.getItems();
+        boolean swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < items.length -1; i++) { //-1 because you have to stop at the second to last item, to be able to compare with next
+               if (items[i].getItem() != null && items[i+1].getItem() != null) { //TODO hvis vi har huller i vores inventory så virker dette ikke
+                   String itemName1 = items[i].getItem().getName();// the current item
+                   String itemName2 = items[i + 1].getItem().getName(); // the item we compare with
+                   if (itemName1.compareTo(itemName2) > 0) { //swap - the compareTo method sorts strings alphabetically by sending -1 if item2 is closer to A 0 if item2 os teh same as item1 and 1 if item2 s closer to z
+                       swapped = true;
+                       //swapping item1 and item2
+                       InventorySlot item1 = items[i];
+                       InventorySlot item2 = items[i + 1];
+                       items[i] = item2;
+                       items[i + 1] = item1;
+                   }
+               }
+            }
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         //tilføj 3 wood arrows
         for (int i = 0; i < 35; i++) {
             inventoryManager.addItem(itemFactory.createWoodArrow());
@@ -41,12 +40,16 @@ public class Main {
                 System.out.println("Cannot add item as there are no empty slots available.");
             }
         }
-
+        System.out.println("------before sorting--------");
+        System.out.println(inventoryManager.printSlotOverview());
+        System.out.println("----------after sorting--------");
+        inventoryManager.dataSort();
         System.out.println(inventoryManager.printSlotOverview());
 
         System.out.println("=== Welcome to the Legend of CodeCraft Inventory! ===");
 
 
+/*
         removeItemFromInventory();
         System.out.println(inventoryManager.printSlotOverview());
         removeItemFromInventory();
@@ -54,7 +57,7 @@ public class Main {
         removeItemFromInventory();
         System.out.println(inventoryManager.printSlotOverview());
         removeItemFromInventory();
-
+*/
         /*
         Hovedmenu:
         1: Add random item to inventory
@@ -65,6 +68,7 @@ public class Main {
         4: Search for items
             (itemSearchMenu())
         5: Sort inventory TODO
+            inventoryManager.dataSort();
         6: Add inventory slots TODO
         7: Advanced features TODO
             herunder:
@@ -75,19 +79,15 @@ public class Main {
         //Search for items menu
 
 
-
         //itemSearchMenu();
-        System.out.println("-----Amalie test----");
-        inventoryManager.searchForItem("Arrow");
-        inventoryManager.searchForItem("Common");
-        inventoryManager.searchForItem("Gloves");
-        inventoryManager.searchForItem("Iron");
+        //System.out.println("-----Amalie test----");
+        //System.out.println(itemSearchMenu());
     }
 
     public static void addRandomItemToInventory() {
         try {
             inventoryManager.addItem(itemFactory.createRandomItem());
-        } catch (InventoryWeightLimitReachedException e){
+        } catch (InventoryWeightLimitReachedException e) {
             System.out.println("Cannot add item as inventory weight limit will be exceeded.");
         } catch (NoEmptySlotsAvailableException e) {
             System.out.println("Cannot add item as there are no empty slots available.");
@@ -102,7 +102,7 @@ public class Main {
             int userInputInt = Integer.parseInt(userInput);
             try {
                 inventoryManager.removeItemFromSlot(userInputInt);
-            } catch (ExceedsAvailableSlotsException e){
+            } catch (ExceedsAvailableSlotsException e) {
                 System.out.println("You do not have access to the chosen slot");
             } catch (InventorySlotAlreadyEmptyException e) {
                 System.out.println("The selected slot is already empty - cannot remove object");
@@ -112,7 +112,7 @@ public class Main {
         }
     }
 
-    public static void itemSearchMenu(){
+    public static String itemSearchMenu() {
         //Search for items using parameters
         System.out.println("Which parameter will you use for your search?\n" +
                 "1: Type\n" +
@@ -122,29 +122,34 @@ public class Main {
         try {
             int userChoice = Integer.parseInt(userInput);
 
-            if (userChoice == 1){ //Searching for Type
+            if (userChoice == 1) { //Searching for Type
                 System.out.println("Which parameter will you use for your search?\n" +
                         "1: Consumable\n" +
                         "2: Weapon\n" +
                         "3: Wearable");
                 userInput = input.nextLine();
                 userChoice = Integer.parseInt(userInput);
-                if (userChoice == 1){ //Searching for Consumables
-                    System.out.println("Which type will you search for?\n " +
+                if (userChoice == 1) { //Searching for Consumables
+                    System.out.println("Which type will you search for?\n" +
                             "1: Health Potion\n" +
                             "2: Arrow\n" +
                             "3: Pellet");
                     userInput = input.nextLine();
                     userChoice = Integer.parseInt(userInput);
-                    if (userChoice > 0 && userChoice < 4) {
-                        //if 1 send health
-                        // if 2 send.... osv
-                        //TODO - kald metode til at håndtere søgning efter consumable
-                        System.out.println("Søger efter consumable X");
+                    if (userChoice > 0 && userChoice < 4) { //Calling search for item in InventoryManager
+                        if (userChoice == 1) {
+                            return inventoryManager.searchForItem("Health Potion");
+                        } else if (userChoice == 2) {
+                            return inventoryManager.searchForItem("Arrow");
+                        } else if (userChoice == 3) {
+                            return inventoryManager.searchForItem("Pellet");
+                        } else {
+                            System.out.println("Please only choose numbers 1 through 3");
+                        }
                     } else {
                         System.out.println("Please only choose numbers 1 through 3");
                     }
-                } else if (userChoice == 2){ //Searching for Weapons
+                } else if (userChoice == 2) { //Searching for Weapons
                     System.out.println("Which type will you search for?\n" +
                             "1: Bow\n" +
                             "2: Sword\n" +
@@ -153,14 +158,21 @@ public class Main {
                     userInput = input.nextLine();
                     userChoice = Integer.parseInt(userInput);
                     if (userChoice > 0 && userChoice < 5) {
-                        //if 1 send Bow
-                        // if 2 send.... osv
-                        //TODO - kald metode til at håndtere søgning efter Weapon
-                        System.out.println("Søger efter Weapon X");
+                        if (userChoice == 1) {
+                            return inventoryManager.searchForItem("Bow");
+                        } else if (userChoice == 2) {
+                            return inventoryManager.searchForItem("Sword");
+                        } else if (userChoice == 3) {
+                            return inventoryManager.searchForItem("Slingshot");
+                        } else if (userChoice == 4) {
+                            return inventoryManager.searchForItem("Shield");
+                        } else {
+                            System.out.println("Please only choose numbers 1 through 4");
+                        }
                     } else {
-                        System.out.println("Please only choose numbers 1 through 3");
+                        System.out.println("Please only choose numbers 1 through 4");
                     }
-                } else if (userChoice == 3){ //Searching for Wearables
+                } else if (userChoice == 3) { //Searching for Wearables
                     System.out.println("Which type will you search for?\n" +
                             "1: Boots\n" +
                             "2: ChestPlate\n" +
@@ -170,17 +182,24 @@ public class Main {
                     userInput = input.nextLine();
                     userChoice = Integer.parseInt(userInput);
                     if (userChoice > 0 && userChoice < 6) {
-                        //if 1 send boots
-                        // if 2 send.... osv
-                        //TODO - kald metode til at håndtere søgning efter Wearable
-                        System.out.println("Søger efter Wearable X");
+                        if (userChoice == 1) {
+                            return inventoryManager.searchForItem("Boots");
+                        } else if (userChoice == 2) {
+                            return inventoryManager.searchForItem("ChestPlate");
+                        } else if (userChoice == 3) {
+                            return inventoryManager.searchForItem("Gloves");
+                        } else if (userChoice == 4) {
+                            return inventoryManager.searchForItem("Helmet");
+                        } else if (userChoice == 5) {
+                            return inventoryManager.searchForItem("Leggings");
+                        } else {
+                            System.out.println("Please only choose numbers 1 through 5");
+                        }
                     } else {
-                        System.out.println("Please only choose numbers 1 through 3");
+                        System.out.println("Please only choose numbers 1 through 5");
                     }
-                } else {
-                    System.out.println("Please only choose numbers 1 through 3");
                 }
-            } else if (userChoice == 2){ //Searching for Material
+            } else if (userChoice == 2) { //Searching for Material
                 System.out.println("Which parameter will you use for your search?\n" +
                         "1: ChainMail\n" +
                         "2: Flint\n" +
@@ -192,8 +211,23 @@ public class Main {
                 userInput = input.nextLine();
                 userChoice = Integer.parseInt(userInput);
                 if (userChoice > 0 && userChoice < 8) {
-                    //TODO - kald metode til at håndtere søgning efter materiale
-                    System.out.println("Søger efter materiale");
+                    if (userChoice == 1) {
+                        return inventoryManager.searchForItem("Chainmail");
+                    } else if (userChoice == 2) {
+                        return inventoryManager.searchForItem("Flint");
+                    } else if (userChoice == 3) {
+                        return inventoryManager.searchForItem("Iron");
+                    } else if (userChoice == 4) {
+                        return inventoryManager.searchForItem("Leather");
+                    } else if (userChoice == 5) {
+                        return inventoryManager.searchForItem("Steel");
+                    } else if (userChoice == 6) {
+                        return inventoryManager.searchForItem("Stone");
+                    } else if (userChoice == 7) {
+                        return inventoryManager.searchForItem("Wood");
+                    } else {
+                        System.out.println("Please only choose numbers 1 through 7");
+                    }
                 } else {
                     System.out.println("Please only choose numbers 1 through 7");
                 }
@@ -206,22 +240,30 @@ public class Main {
                 userInput = input.nextLine();
                 userChoice = Integer.parseInt(userInput);
                 if (userChoice > 0 && userChoice < 5) {
-                    //TODO - kald metode til at håndtere søgning efter rarity
-                    System.out.println("Søger efter rarity");
+                    if (userChoice == 1) {
+                        return inventoryManager.searchForItem("Common");
+                    } else if (userChoice == 2) {
+                        return inventoryManager.searchForItem("Uncommon");
+                    } else if (userChoice == 3) {
+                        return inventoryManager.searchForItem("Rare");
+                    } else if (userChoice == 4) {
+                        return inventoryManager.searchForItem("Legendary");
+                    } else {
+                        System.out.println("Please only choose numbers 1 through 4");
+                    }
                 } else {
-                    System.out.println("Please only choose numbers 1 through 7");
+                    System.out.println("Please only choose numbers 1 through 4");
                 }
-            } else {
-                System.out.println("Please only choose options 1, 2 or 3");
+            }else {
+                System.out.println("Please only choose numbers 1 through 3");
             }
 
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Please only type a whole number");
             //"You have not inputted a valid number"
         }
-    }
-
-    public static void removeItem(){
-
+        return userInput; //TODO ved ikke hvad det her gør
     }
 }
+
+//public static void removeItem(){}
